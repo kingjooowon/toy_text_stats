2600d7a6b0cba35ebc9f8cb1f6b17f249bc3d668
Juwon <a01071597275@gmail.com>
2026-01-17 22:20:57 +0900

    feat: edit count_lines(), count_message() and add get_words()

--- text_stats.py
+++ text_stats.py
@@ -6,50 +6,47 @@ file_path = os.path.join(BASE_DIR, "sample.txt")
 
 with open (file_path, "r", encoding="utf-8") as f:  
     text = f.read()
+    
+def get_words():
+    clean = text.lower()
+    for i in string.punctuation:
+        clean = clean.replace(i, "")
+    
+    return clean.split()
 
 def count_lines(): # 총 줄 수
-    line_count = 0
-    for i in text:
-        if i == ("\n"):
-            line_count += 1
-        else:
-            continue
+    line_count = len(text.splitlines())
     
     return line_count
 
 def count_words(): # 총 단어 수
     word_count = 0
-    words = text.lower()
-    words = words.strip(string.punctuation)
-    words = words.split()
-    
+    words = get_words()
     word_count = len(words)
     
     return word_count
 
 
-def count_message(): # 총 글자 수(공백 미포함)
-    message_count = 0
-    messages = text.lower().strip(" ").strip("\n")
-    message_count = len(messages)
+def count_message(): # 총 글자 수(공백 포함)
+    message = text.replace(" ","").replace("n","")
+    message_count = len(message)
     
     return message_count
 
 def find_longest(): # 가장 긴 단어
     longest_word = ""
-    words = text.lower().strip(string.punctuation).split()
-    for i in range(len(words)):
-        if len(words[i]) > len(longest_word):
-            longest_word = words[i]
-        else:
-            continue
+    words = get_words()
     
+    for j in range(len(words)):
+        if len(words[j]) > len(longest_word):
+            longest_word = words[j]
+            
     return longest_word
 
 def find_average_len(): # 평균 단어 길이
     len_avg = 0.0
     total = 0
-    words = text.lower().strip(string.punctuation).split()
+    words = get_words()
     for i in range(len(words)):
         total += len(words[i])
     
